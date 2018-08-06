2b201507f05b07b0002db273e13e3c093431bd58
gswed <gustav.swedberg@gmail.com>
2018-08-06 16:13:01 +0200

    added selection of similar matches

--- SimpleDictionary/simpleDic.py
+++ SimpleDictionary/simpleDic.py
@@ -1,4 +1,5 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
@@ -6,6 +7,18 @@ def getWord(w):
     w=w.lower()
     if w in data:
         return data[w]
+    elif len(get_close_matches(w, data.keys()))>0:
+        matches = input("Close matches found: %s , Enter X to close, or the index of the desired match: " %get_close_matches(w, data.keys()))
+        if matches == "X" or matches=="x":
+            return "Thank you!"
+        else:
+            try:
+                int(matches)
+                if int(matches)<len(get_close_matches(w, data.keys())):
+                    return data[get_close_matches(w, data.keys())[int(matches)]]
+            except ValueError:
+                return "Sorry, please enter X or the desired index"
+            return "Index out of bounds."
     else:
         return "The word does not exist."
 
